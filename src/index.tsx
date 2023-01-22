0934ceb0ee57c2e380880a09bf18b1a1c6ce10bc
Andrew Wiggin <andrew@wiggin.dev>
2023-01-22 18:37:16 -0500

    Convert exports to server components
    - `ScriptElement` renders the script only if it has not been rendered onto the DOM already. It does this by using  the prop `isGlobal` and a serverside variable. This removes the need for React Context entirely
    - `Balancer` is now partially serverside, rendering its script and `ClientBalancer`. Any code that doesn't explicitly have to run on the client was moved to the server

--- src/index.tsx
+++ src/index.tsx
@@ -1,218 +1,89 @@
-'use client'
-
 import React from 'react'
+import {ClientBalancer, RELAYOUT_STR} from './client'
+import {SYMBOL_KEY} from './constants'
 
-const SYMBOL_KEY = '__wrap_b'
-const SYMBOL_OBSERVER_KEY = '__wrap_o'
-const IS_SERVER = typeof window === 'undefined'
-const useIsomorphicLayoutEffect = IS_SERVER
-  ? React.useEffect
-  : React.useLayoutEffect
+let hasGlobalRelayoutScript = false
 
-interface WrapperElement extends HTMLElement {
-  [SYMBOL_OBSERVER_KEY]?: ResizeObserver | undefined
-}
+const RelayoutScript: React.FC<{ isGlobal?: boolean, suffix?: string }> = ({isGlobal, suffix = ''}) => {
+    const relayoutScript = (
+        <script
+            suppressHydrationWarning
+            dangerouslySetInnerHTML={{
+                // Calculate the balance initially for SSR
+                __html: (hasGlobalRelayoutScript ? '' : `self.${SYMBOL_KEY}=${RELAYOUT_STR};`) + suffix,
+            }}
+        />
+    )
 
-type RelayoutFn = (
-  id: string | number,
-  ratio: number,
-  wrapper?: WrapperElement
-) => void
-
-declare global {
-  interface Window {
-    [SYMBOL_KEY]: RelayoutFn
-  }
-}
-
-const relayout: RelayoutFn = (id, ratio, wrapper) => {
-  wrapper =
-    wrapper || document.querySelector<WrapperElement>(`[data-br="${id}"]`)
-  const container = wrapper.parentElement
-
-  const update = (width: number) => (wrapper.style.maxWidth = width + 'px')
-
-  // Reset wrapper width
-  wrapper.style.maxWidth = ''
-
-  // Get the initial container size
-  const width = container.clientWidth
-  const height = container.clientHeight
-
-  // Synchronously do binary search and calculate the layout
-  let lower: number = width / 2 - 0.25
-  let upper: number = width + 0.5
-  let middle: number
-
-  if (width) {
-    while (lower + 1 < upper) {
-      middle = Math.round((lower + upper) / 2)
-      update(middle)
-      if (container.clientHeight === height) {
-        upper = middle
-      } else {
-        lower = middle
-      }
+    if (isGlobal) {
+        hasGlobalRelayoutScript = true
     }
 
-    // Update the wrapper width
-    update(upper * ratio + width * (1 - ratio))
-  }
-
-  // Create a new observer if we don't have one.
-  // Note that we must inline the key here as we use `toString()` to serialize
-  // the function.
-  if (!wrapper['__wrap_o']) {
-    ;(wrapper['__wrap_o'] = new ResizeObserver(() => {
-      self.__wrap_b(0, +wrapper.dataset.brr, wrapper)
-    })).observe(container)
-  }
+    return relayoutScript
 }
 
-const RELAYOUT_STR = relayout.toString()
-
-const createScriptElement = (injected: boolean, suffix: string = '') => (
-  <script
-    suppressHydrationWarning
-    dangerouslySetInnerHTML={{
-      // Calculate the balance initially for SSR
-      __html: (injected ? '' : `self.${SYMBOL_KEY}=${RELAYOUT_STR};`) + suffix,
-    }}
-  />
+const Provider: React.FC<{
+    children?: React.ReactNode
+}> = ({children}) => (
+    <>
+        <RelayoutScript isGlobal/>
+        {children}
+    </>
 )
 
-interface BalancerProps extends React.HTMLAttributes<HTMLElement> {
-  /**
-   * The HTML tag to use for the wrapper element.
-   * @default 'span'
-   */
-  as?: React.ElementType
-  /**
-   * The balance ratio of the wrapper width (0 <= ratio <= 1).
-   * 0 means the wrapper width is the same as the container width (no balance, browser default).
-   * 1 means the wrapper width is the minimum (full balance, most compact).
-   * @default 1
-   */
-  ratio?: number
-  children?: React.ReactNode
-}
-
-/**
- * An optional provider to inject the global relayout function, so all children
- * Balancer components can share it.
- */
-const BalancerContext = React.createContext<boolean>(false)
-const Provider: React.FC<{
-  children?: React.ReactNode
-}> = ({ children }) => {
-  return (
-    <BalancerContext.Provider value={true}>
-      {createScriptElement(false)}
-      {children}
-    </BalancerContext.Provider>
-  )
+export interface BalancerProps extends React.HTMLAttributes<HTMLElement> {
+    /**
+     * The HTML tag to use for the wrapper element.
+     * @default 'span'
+     */
+    as?: React.ElementType
+    /**
+     * The balance ratio of the wrapper width (0 <= ratio <= 1).
+     * 0 means the wrapper width is the same as the container width (no balance, browser default).
+     * 1 means the wrapper width is the minimum (full balance, most compact).
+     * @default 1
+     */
+    ratio?: number
+    children?: React.ReactNode
 }
 
 const Balancer: React.FC<BalancerProps> = ({
-  as: Wrapper = 'span',
-  ratio = 1,
-  children,
-  ...props
-}) => {
-  const id = React.useId()
-  const wrapperRef = React.useRef<WrapperElement>()
-  const hasProvider = React.useContext(BalancerContext)
+                                               as = 'span',
+                                               ratio = 1,
+                                               children,
+                                               ...props
+                                           }) => {
+    const id = React.useId()
 
-  // Re-balance on content change and on mount/hydration.
-  useIsomorphicLayoutEffect(() => {
-    if (wrapperRef.current) {
-      // Re-assign the function here as the component can be dynamically rendered, and script tag won't work in that case.
-      ;(self[SYMBOL_KEY] = relayout)(0, ratio, wrapperRef.current)
-    }
-  }, [children, ratio])
-
-  // Remove the observer when unmounting.
-  useIsomorphicLayoutEffect(() => {
-    return () => {
-      if (!wrapperRef.current) return
-
-      const resizeObserver = wrapperRef.current[SYMBOL_OBSERVER_KEY]
-      if (!resizeObserver) return
-
-      resizeObserver.disconnect()
-      delete wrapperRef.current[SYMBOL_OBSERVER_KEY]
-    }
-  }, [])
-
-  if (process.env.NODE_ENV === 'development') {
-    // In development, we check `children`'s type to ensure we are not wrapping
-    // elements like <p> or <h1> inside. Instead <Balancer> should directly
-    // wrap text nodes.
-    if (children && !Array.isArray(children) && typeof children === 'object') {
-      if (
-        'type' in children &&
-        typeof children.type === 'string' &&
-        children.type !== 'span'
-      ) {
-        console.warn(
-          `<Balancer> should not wrap <${children.type}> inside. Instead, it should directly wrap text or inline nodes.
+    if (process.env.NODE_ENV === 'development') {
+        // In development, we check `children`'s type to ensure we are not wrapping
+        // elements like <p> or <h1> inside. Instead <Balancer> should directly
+        // wrap text nodes.
+        if (children && !Array.isArray(children) && typeof children === 'object') {
+            if (
+                'type' in children &&
+                typeof children.type === 'string' &&
+                children.type !== 'span'
+            ) {
+                console.warn(
+                    `<Balancer> should not wrap <${children.type}> inside. Instead, it should directly wrap text or inline nodes.
 
 Try changing this:
   <Balancer><${children.type}>content</${children.type}></Balancer>
 To:
   <${children.type}><Balancer>content</Balancer></${children.type}>`
-        )
-      }
-    }
-  }
-
-  return (
-    <>
-      <Wrapper
-        {...props}
-        data-br={id}
-        data-brr={ratio}
-        ref={wrapperRef}
-        style={{
-          display: 'inline-block',
-          verticalAlign: 'top',
-          textDecoration: 'inherit',
-        }}
-        suppressHydrationWarning
-      >
-        {children}
-      </Wrapper>
-      {createScriptElement(hasProvider, `self.${SYMBOL_KEY}("${id}",${ratio})`)}
-    </>
-  )
-}
-
-// As Next.js adds `display: none` to `body` for development, we need to trigger
-// a re-balance right after the style is removed, synchronously.
-if (!IS_SERVER && process.env.NODE_ENV !== 'production') {
-  const next_dev_style = document.querySelector<HTMLElement>(
-    '[data-next-hide-fouc]'
-  )
-  if (next_dev_style) {
-    const callback: MutationCallback = (mutationList) => {
-      for (const mutation of mutationList) {
-        for (const node of Array.from(mutation.removedNodes)) {
-          if (node !== next_dev_style) continue
-
-          observer.disconnect()
-          const elements =
-            document.querySelectorAll<WrapperElement>('[data-br]')
-
-          for (const element of Array.from(elements)) {
-            self[SYMBOL_KEY](0, +element.dataset.brr, element)
-          }
+                )
+            }
         }
-      }
     }
-    const observer = new MutationObserver(callback)
-    observer.observe(document.head, { childList: true })
-  }
-}
+
+    return (
+        <>
+            <ClientBalancer id={id} as={as} ratio={ratio} {...props}>{children}</ClientBalancer>
+            <RelayoutScript suffix={`self.${SYMBOL_KEY}("${id}",${ratio})`}/>
+        </>
+    )
+};
 
 export default Balancer
 export { Provider }
